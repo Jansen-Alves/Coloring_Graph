6f83f2571b09dde2ed91c2dd9e66bcb88452ccfd
Jansen-Alves <75702771+Jansen-Alves@users.noreply.github.com>
2025-04-27 14:28:59 -0300

    adicao de novos arquivos, mHeuristica como código principal

---
+++ main/baseGrafo.py
@@ -0,0 +1,30 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import algorithmx
+import pandas as pd
+
+def grafoImport():
+    G = nx.gnp_random_graph(15, 0.3, 135)
+    # total de vertices e arestas
+    n_vertices = G.number_of_nodes()
+    n_arestas = G.number_of_edges()
+
+    print('vertices: ', n_vertices, '\narestas: ', n_arestas)
+    pares = G.edges()
+    no_1 = []
+    no_2 = []
+    for u, v in pares:
+        no_1.append(u)
+        no_2.append(v)
+
+    #for i in range(len(no_1)):
+    #    print('par', i,':(',no_1[i],',',no_2[i],')')
+
+    nx.draw_circular(G, with_labels=True)
+    nx.write_edgelist(
+        G,
+        "grafoBase.csv",
+        delimiter = ",",
+        data = False,
+        encoding ='utf-8'
+    )
